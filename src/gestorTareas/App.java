807371fdee373493050a35b55c2083967e8e37d0
Axel <axelpartida584@gmail.com>
2025-04-07 18:44:54 -0600

    Terminando el menu principal

--- src/gestorTareas/App.java
+++ src/gestorTareas/App.java
@@ -1,6 +1,13 @@
 //Donde se manejará la logica de la app.
 package gestorTareas;
 
+import static gestorTareas.Consola.*;
+
 public class App {
-    
+    public static void app(){
+        int opcion;
+        do{
+            opcion = menuPrincipal();
+        }while(opcion != 6);
+    }
 }
--- src/gestorTareas/Consola.java
+++ src/gestorTareas/Consola.java
@@ -1,6 +1,25 @@
 //Donde se manejaran las entradas de datos
 package gestorTareas;
+import java.util.Scanner;
 
 public class Consola {
+    private static int opcion;
+    private static Scanner sc = new Scanner(System.in);
     
+    public static int menuPrincipal(){ //Funcion que te muestra el menu y retorna la opcion a seguir
+        System.out.println("MENU");
+        try{
+            do{
+                System.out.println("1. REGISTRAR NUEVA TAREA\n2. MOSTRAR TODAS LAS TAREAS\n3. FILTRAR TAREA POR USUARIO\n4. MARCAR TAREA COMO HECHA\n5. ELIMINAR TAREAS\n6. SALIR");
+                opcion = sc.nextInt();
+                if(opcion < 1 || opcion > 6){
+                    System.out.println("Digita un valor valido");
+                }
+            }while(opcion < 1 || opcion > 6);
+        }catch(Exception e){
+            System.out.println("Digita un valor valido");
+            sc.nextLine();
+        }
+        return opcion;
+    }
 }
