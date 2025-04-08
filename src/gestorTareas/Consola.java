36e045525a16332782ece8c46821bb1f8df7bf98
Axel <axelpartida584@gmail.com>
2025-04-07 19:53:49 -0600

    Validacion del menu principal arreglada

--- src/gestorTareas/Consola.java
+++ src/gestorTareas/Consola.java
@@ -8,19 +8,19 @@ public class Consola {
     
     public static int menuPrincipal(){ //Funcion que te muestra el menu y retorna la opcion a seguir
         System.out.println("MENU");
-        try{
-            do{
+        do{
+            try{
                 System.out.println("1. REGISTRAR NUEVA TAREA\n2. MOSTRAR TODAS LAS TAREAS\n3. FILTRAR TAREA POR USUARIO\n4. MARCAR TAREA COMO HECHA\n5. ELIMINAR TAREAS\n6. SALIR");
                 opcion = sc.nextInt();
                 sc.nextLine();
                 if(opcion < 1 || opcion > 6){
                     System.out.println("Digita un valor valido");
                 }
-            }while(opcion < 1 || opcion > 6);
-        }catch(Exception e){
-            System.out.println("Digita un valor valido");
-            sc.nextLine();
-        }
+            }catch(Exception e){
+                System.out.println("Digita un valor valido");
+                sc.nextLine();
+            }
+        }while(opcion < 1 || opcion > 6);
         return opcion;
     }
     
