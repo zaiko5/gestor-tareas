202ba75b4d62b48da6094bf8a36f3efef7cd45d8
Axel <axelpartida584@gmail.com>
2025-04-07 18:27:53 -0600

    Terminando la conexion de la BBDD

--- src/gestorTareas/Main.java
+++ src/gestorTareas/Main.java
@@ -1,8 +1,22 @@
 //Clase en donde se correra la aplicacion
 package gestorTareas;
+import java.sql.*;
 
 public class Main {
     public static void main(String [] args){
-        
+        try{
+            Class.forName("org.sqlite.JDBC"); //Conectando con el archivo jar de sqlite
+            String url = "jdbc:sqlite:C:/Users/HP/Desktop/o/PROYECTOS/gestorTareas/tareasDB.db"; //Direccion url de la base de datos
+            try (Connection conn = DriverManager.getConnection(url)) { //Representando la conexcion con un obejto connection.
+                if (conn != null) { //Si la conexion funciona
+                    //Agregar la logica para la app.
+                    conn.close();
+                }
+            } catch(SQLException e) { //Atrapamos la excepcion del tipo SQL si no se pudo conectar con la DB
+                System.out.println("Error al conectar la base de datos");
+            }
+        }catch(ClassNotFoundException e){
+            System.out.println("Error al encontrar el driver de la BBDD");
+        }
     }
 }
