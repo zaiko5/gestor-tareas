1dd4a7e4c51e9ebf5a443947b4e3719efb303103
Axel <axelpartida584@gmail.com>
2025-04-07 19:00:37 -0600

    Arreglado lo de la suma de los dias a un objeto localdate

--- src/gestorTareas/Tarea.java
+++ src/gestorTareas/Tarea.java
@@ -1,6 +1,36 @@
 //Para maneja las tareas
 package gestorTareas;
+import java.time.LocalDate;
 
 public class Tarea {
-    
+    //Atributos de la clase tarea
+    private String descripcion;
+    private boolean estado;
+    private LocalDate fechaInicial;
+    private LocalDate fechaFinal;
+    private String responsable;
+
+    public Tarea(String descripcion, String responsable, int dias) {
+        this.descripcion = descripcion;
+        this.responsable = responsable;
+        this.estado = false;
+        this.fechaInicial = LocalDate.now(); //Obteniendo la fecha del dia de la tarea
+        this.fechaFinal = this.fechaInicial.plusDays(dias); //Obteniendo la fecha final a partir de los dias ingresados
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public LocalDate getFecha() {
+        return fechaInicial;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
 }
